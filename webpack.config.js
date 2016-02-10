0f01b7ad56662ff9351a8fc0471e431257b7ec00
cubedro <contact@siteshop.ro>
2016-02-10 05:03:09 +0200

    fixed travis-ci errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,50 @@
+var path = require('path');
 var webpack = require('webpack');
+var nodeModulesPath = path.resolve(__dirname, 'node_modules');
 
 module.exports = {
-    entry: './src/index',
-    module: {
-        loaders: [
-            { test: /\.js$/, loader: 'babel', exclude: /node_modules/ }
-        ]
-    },
-    output: {
-        filename: 'dist/uport-sso.min.js',
-        libraryTarget: 'umd',
-        library: 'uport-sso'
-    },
-    plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compressor: {
-                warnings: false
-            }
-        })
-    ]
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '/uport-sso.min.js',
+    libraryTarget: 'umd',
+    library: 'uport-sso.js',
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json'],
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+      },
+    }),
+  ],
+  resolveLoader: {
+    fallback: path.join(__dirname, 'node_modules'),
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: [nodeModulesPath],
+        include: path.join(__dirname, 'src'),
+        query: {
+          presets: ['es2015', 'stage-0'],
+        },
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+        exclude: [nodeModulesPath],
+      },
+    ],
+  },
 };
